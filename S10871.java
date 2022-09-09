e2b10fffc205570f045c189dbea82458b43e53b2
hyejinme <kimhyejin703@daum.net>
2022-09-10 08:43:37 +0900

    10871번 - X보다 작은 수

---
+++ S10871.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class S10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            int A = sc.nextInt();
+            if(X > A){
+                System.out.print(A+" ");
+            }
+        }
+    }
+}
