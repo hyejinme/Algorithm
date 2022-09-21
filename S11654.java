c17717675bd5e3a627cbb23f7b25bcd660bfbbf0
hyejinme <kimhyejin703@daum.net>
2022-09-21 16:50:41 +0900

    11654번 - 아스키 코드

---
+++ S11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+//11654번 - 아스키 코드
+public class S11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.next().charAt(0);
+        System.out.println(s);
+    }
+}
